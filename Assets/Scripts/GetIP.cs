992759500c13cadd48bfaf1e1ec8079493df2fd4
Eduardo Soto <easm93@gmail.com>
2016-03-14 23:48:10 -0400

    First commit

---
+++ Assets/Scripts/GetIP.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class GetIP : NetworkBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+		string ip = Network.player.ipAddress;
+		GameObject.Find ("NetworkManager").GetComponent<NetworkManager> ().networkAddress = ip;
+	}
+
+}
---
+++ Assets/Scripts/PlayerNetworkInit.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class PlayerNetworkInit : NetworkBehaviour {
+
+	[SerializeField] GameObject FPSC;
+
+	// Use this for initialization
+	void Start () {
+		if (isLocalPlayer) {
+			GameObject.Find ("Scene Camera").SetActive (false);
+			GetComponent<UnityStandardAssets.Characters.FirstPerson.RigidbodyFirstPersonController> ().enabled = true;
+			FPSC.SetActive (true);
+		}
+	}
+
+}
